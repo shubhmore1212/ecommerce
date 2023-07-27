5028cfb8ab68a7c5b5434797a5888c60733fa769
shubham <shubhmore97@gmail.com>
2023-07-27 19:57:57 +0530

    feature: shopping cart UI added

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,7 +1,58 @@
 import React from "react";
+import "./Cart.scss";
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  const totalPrice = () => {
+    let total = 0;
+    products.forEach((item) => {
+      total += item.quantity * item.price;
+    });
+    return total.toFixed(2);
+  };
+
+  const handlePayment = async () => {};
+
+  const products = [
+    {
+      id: 1,
+      img: "",
+      title: "Shirt",
+      desc: "the shirt",
+      quantity: 2,
+      price: 100,
+    },
+  ];
+
+  return (
+    <div className="cart">
+      <h1>Products in your cart</h1>
+      {products?.map((item) => (
+        <div className="item" key={item.id}>
+          {/* <img src={process.env.REACT_APP_UPLOAD_URL + item.img} alt="" /> */}
+          <div className="details">
+            <h1>{item.title}</h1>
+            <p>{item.desc?.substring(0, 100)}</p>
+            <div className="price">
+              {item.quantity} x ${item.price}
+            </div>
+          </div>
+          <DeleteOutlinedIcon
+            className="delete"
+            onClick={() => console.log("delete")}
+          />
+        </div>
+      ))}
+      <div className="total">
+        <span>SUBTOTAL</span>
+        <span>${totalPrice()}</span>
+      </div>
+      <button onClick={handlePayment}>PROCEED TO CHECKOUT</button>
+      <span className="reset" onClick={() => console.log("reset")}>
+        Reset Cart
+      </span>
+    </div>
+  );
 };
 
 export default Cart;
